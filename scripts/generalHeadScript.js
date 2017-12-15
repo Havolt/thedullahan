ef23d6c821b9974cd5e42fe6fa0f2db6e43b6dfb
Mark Fitzpatrick <markfitz815@gmail.com>
2017-12-15 16:45:39 +0000

    Creating header

--- scripts/generalHeadScript.js
+++ scripts/generalHeadScript.js
@@ -1,1 +1,15 @@
 console.log('lol');
+
+function createHeader(){
+    const hd = document.createElement('div');
+    hd.classList += 'mainHead';
+    site.appendChild(hd);
+    const hdTitle = document.createElement('h1');
+    hdTitle.classList += 'mainHeadTitle';
+    hdTitle.innerHTML="theDullahan";
+    hd.appendChild(hdTitle);
+}
+
+(function initHead(){
+    createHeader();
+})()
