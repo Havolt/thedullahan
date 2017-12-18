413076d5e46d7c9611fb18ce52b79367d6886ac0
Mark Fitzpatrick <markfitz815@gmail.com>
2017-12-18 20:15:44 +0000

    added comments to javascript of header/ trying out onresize event listner

--- scripts/generalHeadScript.js
+++ scripts/generalHeadScript.js
@@ -2,16 +2,25 @@ window.onload = function(){
     console.log('lol');
 
 
+//Make the header html
 function createHeader(){
+    //The wrapping div for header
+    const fullHead = document.createElement('div');
+    site.appendChild(fullHead);
+    fullHead.id = 'fullHead';
+    //Array with links for the the list
     const linkNames = ['Home', 'Legends', 'Stories', 'Reviews',  'About', ];
+    //Main div of header
     const hd = document.createElement('div');
     hd.classList += 'mainHead';
-    site.appendChild(hd);
+    fullHead.appendChild(hd);
+    //Title of the website
     const hdTitle = document.createElement('h1');
     hdTitle.classList += 'mainHeadTitle';
     hdTitle.innerHTML="theDullahan";
     hdTitle.addEventListener('click', () => window.open('index.html', "_self"));
     hd.appendChild(hdTitle);
+    //Creates the links in the header by looping through linkNames array
     const hdLinks = document.createElement('ul');
     hdLinks.classList += 'headerLinks'
     for(let i = 0; i < linkNames.length; i++){
@@ -28,6 +37,10 @@ function createHeader(){
     }
     hd.appendChild(hdLinks);
 }
+
+window.addEventListener('resize', function(){console.log($(window).width())});
+
+ 
 
 (function initHead(){
     createHeader();
