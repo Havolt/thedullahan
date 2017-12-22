7249c906c892b86192439da368f3df7cf23d15f8
Mark Fitzpatrick <markfitz815@gmail.com>
2017-12-22 16:03:59 +0000

    Testing the use of implementing paragraphs by looping through array with strings

--- scripts/aboutGen.js
+++ scripts/aboutGen.js
@@ -2,14 +2,19 @@ const currSection = 'about';
 
 
 //Strings for paragraphs
-const aboutOpenPText = "Hello there world how are you?"
+const aboutOpenText1 = "From a young age I've been fascinated with the paranormal, conspiracy theories and cryptozoology. From tales told to me by my grnadmother to a curious book of conspiracy theories I found in an old dusty book store a few days before it closed these worlds just out of sight have fascinated me."
+const aboutOpenText2 = "As I grew into my teens shows started airing on Television about unresolved mysteries and strange serial killers. I was digsuted and frightened by a lot of what I saw but also fascinated at the same time."
 
+
+const aboutOpenTextArr = [aboutOpenText1, aboutOpenText2]
 
 //Opening container Div
 const aboutOpenContain = document.createElement('div');
 siteMain.appendChild(aboutOpenContain);
 
 //Opening Paragraph
-const aboutOpenP = document.createElement('p');
-aboutOpenP.innerHTML=aboutOpenPText;
-aboutOpenContain.appendChild(aboutOpenP);
+for(var i = 0; i < aboutOpenTextArr.length; i++){
+    const aboutOpenP = document.createElement('p');
+    aboutOpenP.innerHTML=aboutOpenTextArr[i];
+    aboutOpenContain.appendChild(aboutOpenP);
+}
