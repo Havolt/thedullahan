f686df02216df0f18d70bb0ea268f4137cb7296e
Mark Fitzpatrick <markfitz815@gmail.com>
2017-12-22 15:27:58 +0000

    created images folder, aboutMain section now being worked on

--- scripts/aboutGen.js
+++ scripts/aboutGen.js
@@ -1,1 +1,15 @@
 const currSection = 'about';
+
+
+//Strings for paragraphs
+const aboutOpenPText = "Hello there world how are you?"
+
+
+//Opening container Div
+const aboutOpenContain = document.createElement('div');
+siteMain.appendChild(aboutOpenContain);
+
+//Opening Paragraph
+const aboutOpenP = document.createElement('p');
+aboutOpenP.innerHTML=aboutOpenPText;
+aboutOpenContain.appendChild(aboutOpenP);
