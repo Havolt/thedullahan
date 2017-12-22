caccaab70b3d6c3c5b0cd8cdb4f333266b39cb7a
Mark Fitzpatrick <markfitz815@gmail.com>
2017-12-22 13:49:47 +0000

    Added correct divs to all html pages and styled width of main part of website

--- scripts/homeGen.js
+++ scripts/homeGen.js
@@ -1,7 +1,9 @@
 const currSection = 'home';
 
+/*
 const testdiv = document.createElement('div');
 testdiv.style.width="200px";
 testdiv.style.height="200px";
 testdiv.style.background="blue";
 siteMain.appendChild(testdiv);
+*/
